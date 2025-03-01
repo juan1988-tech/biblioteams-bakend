352875f56d5bfd3bc5935da9a60d0d73af5253a9
juan1988-tech <jfrancoh5@gmail.com>
2025-02-28 19:42:43 -0500

    rutas de prueba

--- index.mjs
+++ index.mjs
@@ -1,6 +1,8 @@
 import connection  from './database/connection.mjs';
 import express from 'express';
-import cors  from 'cors'
+import cors  from 'cors';
+import bookRouter from './routes/book.mjs';
+import userRouter from './routes/user.mjs';
 
 /*  💡 establecer la conexión al servidor ✨ */
 connection();
@@ -18,3 +20,17 @@ const port = 3002;
 app.listen(port,()=>{
     console.log(` 🖥 Servidor corriendo en el puerto ${port}`)
 })
+
+/*probar ruta de prueba a la base de datos*/
+app.get('/probando',(req,res)=>{
+    console.log('Estoy ejecutando la ruta')
+
+    res.status(200).json({
+        status:"succcess",
+        message:"ruta de prueba"
+    })
+})
+
+/*configuración de las rutas para los libros*/
+app.use('/api',bookRouter)
+app.use('/api',userRouter)
